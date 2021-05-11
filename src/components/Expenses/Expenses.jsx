2b1b59aface4a08e0d2c4712312926ab4d8b0c9d
Giovanni Bronca <42703789+gbronca@users.noreply.github.com>
2021-05-11 10:24:07 +0100

    Created ExpenseList component to render filtered items

--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
-import ExpenseItem from './ExpenseItem';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 import Card from '../UI/Card';
 
 import './Expenses.css';
@@ -17,19 +17,6 @@ const Expenses = (props) => {
 		return expense.date.getFullYear().toString() === filteredYear;
 	});
 
-	let expensesContent = <p>No expenses found</p>;
-
-	if (filteredExpenses.length > 0) {
-		expensesContent = filteredExpenses.map((expense) => (
-			<ExpenseItem
-				key={expense.id}
-				title={expense.title}
-				amount={expense.amount}
-				date={expense.date}
-			/>
-		));
-	}
-
 	return (
 		<div>
 			<Card className='expenses'>
@@ -37,34 +24,7 @@ const Expenses = (props) => {
 					selected={filteredYear}
 					onChangeFilter={filterChangeHandler}
 				/>
-				{/* Preferred method as it is easier to read.
-				if the first part is true, javascript returns what is
-				after the && operator */}
-				{/* {filteredExpenses.length === 0 && <p>No expenses found</p>}
-				{filteredExpenses.length > 0 &&
-					filteredExpenses.map((expense) => (
-						<ExpenseItem
-							key={expense.id}
-							title={expense.title}
-							amount={expense.amount}
-							date={expense.date}
-						/>
-					))} */}
-
-				{/* Using ternary operator, but not the preferred method */}
-				{/* {filteredExpenses.length === 0 ? (
-					<p>No expenses found</p>
-				) : (
-					filteredExpenses.map((expense) => (
-						<ExpenseItem
-							key={expense.id}
-							title={expense.title}
-							amount={expense.amount}
-							date={expense.date}
-						/>
-					))
-				)} */}
-				{expensesContent}
+				<ExpensesList items={filteredExpenses} />
 			</Card>
 		</div>
 	);
